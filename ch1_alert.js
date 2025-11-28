c24a9cc7310b16895a262beb59e06aada3fa6682
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2025-11-28 04:40:25 +0300

    using built in method typeof

--- ch1_alert.js
+++ ch1_alert.js
@@ -1,1 +1,14 @@
 alert("Saying hi from external JS file!");
+
+let empty = null;
+let strToNr2 = "hello";
+strToNr2 = Number(strToNr2);
+console.log(strToNr2 , typeof strToNr2 ); // NaN
+
+let strToBool2 = "false";
+strToBool2 = Boolean(strToBool2);
+console.log(strToBool2 , typeof strToBool2 ); // true
+
+let strToBool = "";
+strToBool = Boolean(strToBool);
+console.log(strToBool , typeof strToBool ); // false
